e85398d98a02e5d5f56935ba8e612dc703ece16f
Josemar Luedke <josemarluedke@gmail.com>
2014-03-03 20:06:30 -0300

    Fix balanced_contributor class_name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ActiveRecord::Base
   # TODO "Neighborly::" can be removed after the project rename to "Neighborly"
-  has_one :balanced_contributor, class_name: 'Neighborly::Balanced'
+  has_one :balanced_contributor, class_name: 'Neighborly::Balanced::Contributor'
 end
