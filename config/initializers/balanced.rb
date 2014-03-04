df1b18af18a3d805088224fcdf1e352f457cd2bd
Irio Irineu Musskopf Junior <iirineu@gmail.com>
2014-03-04 12:03:07 -0300

    Let server come up without balanced_api_key_secret configuration
    When someone is starting the app for the first time (like running
    "rails console"), the server must be able to come up to let the user to
    run commands without defining configuration for Balanced.

--- config/initializers/balanced.rb
+++ config/initializers/balanced.rb
@@ -1,2 +1,4 @@
 options = Rails.env.development? ? { logging_level: 'INFO' } : {}
-Balanced.configure(Configuration.fetch(:balanced_api_key_secret), options)
+Rails.logger.warn 'Set a value for Balanced\'s API secret with '\
+                  '"Configuration[:balanced_api_key_secret] = \'foo_bar\' and restart the server.'
+Balanced.configure(Configuration[:balanced_api_key_secret], options)
